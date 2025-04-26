9c5a908da0db46877e3b6a4411a2755128db9baf
Yihang Zhu <yihangz@umich.edu>
2025-04-26 14:45:33 -0400

    Add comment mapping.

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -7,3 +7,14 @@ vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'Insert mode move right' })
 vim.keymap.set('i', '<C-j>', '<Up>', { desc = 'Insert mode move up' })
 vim.keymap.set('i', '<C-k>', '<Down>', { desc = 'Insert mode move down' })
 vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Open file [e]xplorer' })
+local map = vim.keymap.set
+
+-- Normal 模式下 Ctrl + / 注释当前行
+map('n', '<C-_>', function()
+  require('Comment.api').toggle.linewise.current()
+end, { desc = '注释/取消注释当前行' })
+
+-- Visual 模式下 Ctrl + / 注释选中内容
+map('v', '<C-_>', function()
+  require('Comment.api').toggle.linewise(vim.fn.visualmode())
+end, { desc = '注释/取消注释选中内容' })
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup {
+        contrast = 'hard',
+      }
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  { 'numToStr/Comment.nvim', opts = {} },
+}
