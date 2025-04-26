fd03532ef6309fb24a765485b3f8327278f9162f
Yihang Zhu <yihangz@umich.edu>
2025-04-26 15:32:02 -0400

    Modification on comments.

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -9,12 +9,10 @@ vim.keymap.set('i', '<C-k>', '<Down>', { desc = 'Insert mode move down' })
 vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Open file [e]xplorer' })
 local map = vim.keymap.set
 
--- Normal 模式下 Ctrl + / 注释当前行
 map('n', '<C-_>', function()
   require('Comment.api').toggle.linewise.current()
-end, { desc = '注释/取消注释当前行' })
+end, { desc = 'Comment/Uncomment the selected content' })
 
--- Visual 模式下 Ctrl + / 注释选中内容
 map('v', '<C-_>', function()
   require('Comment.api').toggle.linewise(vim.fn.visualmode())
-end, { desc = '注释/取消注释选中内容' })
+end, { desc = 'Comment/Uncomment the selected content' })
